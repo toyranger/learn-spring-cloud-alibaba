378aa1eb5f9fbdf06d95f6e988359085a166a3d9
toyranger <toy_shen@163.com>
2020-01-03 23:47:42 +0800

    chins: nacos 分布式配置中心动态刷新

--- learn-spring-cloud-alibaba-consumer/src/main/java/com/chins/spring/cloud/alibaba/consumer/controller/HelloController.java
+++ learn-spring-cloud-alibaba-consumer/src/main/java/com/chins/spring/cloud/alibaba/consumer/controller/HelloController.java
@@ -2,18 +2,29 @@ package com.chins.spring.cloud.alibaba.consumer.controller;
 
 import com.chins.spring.cloud.alibaba.consumer.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 
     @Autowired
     private HelloService helloService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping("/feign/hello/{name}")
     public String hello(@PathVariable String name) {
         return helloService.hello(name);
+    }
+
+    @GetMapping("/feign/echo")
+    public String echo() {
+        return username;
     }
 }
