4abe505fd9c27b9f0ef3255d24092b52b7b4a3e8
toyranger <toy_shen@163.com>
2020-01-04 16:05:05 +0800

    chins: sentinel熔断模型完成

---
+++ learn-spring-cloud-alibaba-consumer/src/main/java/com/chins/spring/cloud/alibaba/consumer/fallback/HelloServiceFallBack.java
@@ -0,0 +1,12 @@
+package com.chins.spring.cloud.alibaba.consumer.fallback;
+
+import com.chins.spring.cloud.alibaba.consumer.service.HelloService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloServiceFallBack implements HelloService {
+    @Override
+    public String hello(String name) {
+        return "你的网络有问题";
+    }
+}
--- learn-spring-cloud-alibaba-consumer/src/main/java/com/chins/spring/cloud/alibaba/consumer/service/HelloService.java
+++ learn-spring-cloud-alibaba-consumer/src/main/java/com/chins/spring/cloud/alibaba/consumer/service/HelloService.java
@@ -1,10 +1,11 @@
 package com.chins.spring.cloud.alibaba.consumer.service;
 
+import com.chins.spring.cloud.alibaba.consumer.fallback.HelloServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-provider")
+@FeignClient(name = "service-provider", fallback = HelloServiceFallBack.class)
 public interface HelloService {
 
     @GetMapping("/hello/{name}")
