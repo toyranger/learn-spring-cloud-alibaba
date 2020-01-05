7c0aeb29981c0c9a78e19180dd31b121d66b6ecd
toyranger <toy_shen@163.com>
2020-01-05 21:13:56 +0800

    chins: oauth2 jdbc模式完成

--- learn-spring-cloud-alibaba-oauth2/learn-spring-cloud-alibaba-oauth2-server/src/main/java/com/chins/spring/cloud/alibaba/oauth/server/config/AuthorizationConfiguration.java
+++ learn-spring-cloud-alibaba-oauth2/learn-spring-cloud-alibaba-oauth2-server/src/main/java/com/chins/spring/cloud/alibaba/oauth/server/config/AuthorizationConfiguration.java
@@ -1,11 +1,21 @@
 package com.chins.spring.cloud.alibaba.oauth.server.config;
 
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -14,11 +24,41 @@ public class AuthorizationConfiguration extends AuthorizationServerConfigurerAda
   @Autowired
   private BCryptPasswordEncoder passwordEncoder;
 
+  @Bean
+  @Primary
+  @ConfigurationProperties(prefix = "spring.datasource")
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JdbcTokenStore(dataSource());
+  }
+
+  @Bean
+  public ClientDetailsService jdbcClientDetailsService() {
+    return new JdbcClientDetailsService(dataSource());
+  }
+
   @Override
   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-    clients.inMemory().withClient("client").secret(passwordEncoder.encode("secret"))
-        .authorizedGrantTypes("authorization_code").scopes("app")
-        .redirectUris("https://www.baidu.com");
+    clients.withClientDetails(jdbcClientDetailsService());
   }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+    endpoints.tokenStore(tokenStore());
+  }
+
+//  @Override
+//  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//
+//    clients.inMemory().withClient("client").secret(passwordEncoder.encode("secret"))
+//        .authorizedGrantTypes("authorization_code").scopes("app")
+//        .redirectUris("https://www.baidu.com");
+//  }
+
+
 }
---
+++ learn-spring-cloud-alibaba-oauth2/learn-spring-cloud-alibaba-oauth2-server/src/test/java/com/chins/spring/cloud/alibaba/oauth/server/test/TestOAuth2AuthenAndAuthorApplication.java
@@ -0,0 +1,26 @@
+package com.chins.spring.cloud.alibaba.oauth.server.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestOAuth2AuthenAndAuthorApplication {
+
+  @Autowired
+  private BCryptPasswordEncoder passwordEncoder;
+
+  @Test
+  public void test() {
+    System.out.println(passwordEncoder.encode("secret"));
+  }
+
+  @Test
+  public void testDatasource() {
+
+  }
+}
